44434661910d9c2b7df4b712fd96c9b85066000d
Murat <mdenizalioglu@gmail.com>
2022-07-10 13:56:34 +0300

    Create model: News

---
+++ newsapp-backend/src/main/java/com/example/newsapp/model/News.java
@@ -0,0 +1,38 @@
+package com.example.newsapp.model;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    Integer id;
+
+    @Column
+    String title;
+
+    @Column
+    String content;
+
+    @Column
+    String imageUrl;
+
+    @Column
+    Timestamp createTime = new Timestamp(System.currentTimeMillis());
+
+    @Column
+    Timestamp updateTime;
+
+    @Column
+    Timestamp deleteTime;
+
+    @Column
+    Boolean deleted = false;
+}
