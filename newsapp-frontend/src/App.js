a5ff2e3a634f632cdb9a64b23dbb3f8290cc8068
Murat <mdenizalioglu@gmail.com>
2022-07-11 21:17:26 +0300

    Create Frontend project

---
+++ newsapp-frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { news: [] }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/news", {
+      headers: {
+        Accept: 'application/json'
+      }
+    }).then(res => res.json())
+    .then(data => this.setState({ news: data }))
+  }
+
+  render() {
+    return <div>
+      <ul>
+        {this.state.news.map(n => 
+          <li key={n.id}>
+            <h3>{n.title}</h3>
+            <p>{n.content}</p>
+            <p><img src={n.imageUrl} /></p>
+          </li>)
+        }
+      </ul>
+      </div>
+  }
+}
+
+export default App;
